a9d705a53d756b0d86c39de43cd518d2d0aaeb8f
Baptiste Manson <baptiste.manson@gmail.com>
2021-01-14 22:46:15 -0800

    step 2 of builder done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,53 @@
+use core::panic;
+
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let original_id = input.ident;
+    let builder_id = Ident::new(&format!("{}Builder", original_id), Span::call_site());
+    let s = match input.data {
+        Data::Struct(s) => s,
+        _ => panic!("Only accept struct"),
+    };
 
-    unimplemented!()
+    let builder_fields: Vec<proc_macro2::TokenStream> = s
+        .fields
+        .iter()
+        .map(|s| {
+            let name = &s.ident;
+            let ty = &s.ty;
+            quote! { #name: Option<#ty> }
+        })
+        .collect();
+
+    let default_builder_values: Vec<proc_macro2::TokenStream> = s
+        .fields
+        .iter()
+        .map(|s| {
+            let name = &s.ident;
+            quote! { #name: None }
+        })
+        .collect();
+
+    let expanded = quote! {
+       impl #original_id {
+         fn builder() -> #builder_id {
+            #builder_id {
+                #(#default_builder_values,)*
+            }
+         }
+       }
+        pub struct #builder_id {
+            #(#builder_fields,)*
+        }
+    };
+
+    // Hand the output tokens back to the compiler
+    TokenStream::from(expanded)
 }
