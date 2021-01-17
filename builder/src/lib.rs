942d811771f5cf774d838842e1d58b475e76ae0c
Baptiste Manson <baptiste.manson@gmail.com>
2021-01-16 22:25:39 -0800

    step 6 done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,32 @@
-use core::panic;
-
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, Data, DeriveInput};
+
+fn is_type_option(ty: &syn::Type) -> bool {
+    match ty {
+        syn::Type::Path(p) => p.path.segments[0].ident == "Option",
+        _ => false,
+    }
+}
+
+fn get_wrapped_type_inside_option(ty: &syn::Type) -> syn::Type {
+    match ty {
+        syn::Type::Path(p) => match &p.path.segments[0].arguments {
+            syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                args,
+                ..
+            }) => {
+                if let syn::GenericArgument::Type(optional_type) = &args[0] {
+                    optional_type.clone()
+                } else {
+                    panic!("problem finding a type")
+                }
+            }
+            _ => panic!("only works on option type"),
+        },
+        _ => panic!("only works on option type"),
+    }
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -15,23 +39,55 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_id = format_ident!("{}Builder", original_id);
 
-    let field_names: Vec<proc_macro2::TokenStream> = s
+    let all_fields: Vec<(bool, proc_macro2::TokenStream, &syn::Type)> = s
         .fields
         .iter()
         .map(|s| {
             let name = &s.ident;
-            quote! { #name }
-        })
-        .collect();
-
-    let field_types: Vec<proc_macro2::TokenStream> = s
-        .fields
-        .iter()
-        .map(|s| {
             let ty = &s.ty;
-            quote! { #ty }
+            (is_type_option(&s.ty), quote! { #name }, ty)
         })
         .collect();
+    let (all_names, _all_types): (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>) =
+        all_fields
+            .iter()
+            .map(|f| {
+                let ty = f.2;
+                (f.1.clone(), quote! { #ty })
+            })
+            .unzip();
+
+    let (mandatory_names, mandatory_types): (
+        Vec<proc_macro2::TokenStream>,
+        Vec<proc_macro2::TokenStream>,
+    ) = all_fields
+        .iter()
+        .filter_map(|f| {
+            if f.0 {
+                None
+            } else {
+                let ty = f.2;
+                Some((f.1.clone(), quote! { #ty }))
+            }
+        })
+        .unzip();
+
+    let (optional_names, optional_types): (
+        Vec<proc_macro2::TokenStream>,
+        Vec<proc_macro2::TokenStream>,
+    ) = all_fields
+        .iter()
+        .filter_map(|f| {
+            if !f.0 {
+                None
+            } else {
+                let ty = get_wrapped_type_inside_option(&f.2);
+                Some((f.1.clone(), quote! { #ty}))
+            }
+        })
+        .unzip();
+    // identify all optional types and all non optional types
+    // will be useful for the build function
 
     let expanded = quote! {
     #[derive(Debug)]
@@ -49,28 +105,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
     }
 
             pub struct #builder_id {
-                #(#field_names: Option<#field_types>,)*
+                #(#mandatory_names: Option<#mandatory_types>,)*
+                #(#optional_names: Option<#optional_types>,)*
             }
 
             impl #original_id {
                  fn builder() -> #builder_id {
                     #builder_id {
-                        #(#field_names : None,)*
+                        #(#all_names : None,)*
                     }
                 }
             }
 
 
             impl #builder_id {
-                #(pub fn #field_names(&mut self, #field_names: #field_types) -> &mut Self {
-                    self.#field_names = Some(#field_names);
+                #(pub fn #mandatory_names(&mut self, #mandatory_names: #mandatory_types) -> &mut Self {
+                    self.#mandatory_names = Some(#mandatory_names);
+                    self
+                })*
+
+                #(pub fn #optional_names(&mut self, #optional_names: #optional_types) -> &mut Self {
+                    self.#optional_names = Some(#optional_names);
                     self
                 })*
 
                 pub fn build(&mut self) -> Result<#original_id, Box<dyn std::error::Error>> {
-                    #(let #field_names = self.#field_names.clone().ok_or(BuilderError {})?;)*
+                    #(let #mandatory_names = self.#mandatory_names.clone().ok_or(BuilderError {})?;)*
+                    #(let #optional_names = self.#optional_names.clone();)*
                     Ok(#original_id {
-                        #(#field_names: #field_names.clone(),)*
+                        #(#all_names: #all_names,)*
                     })
                 }
             }
